14a97dcb3dd8449ecb3efbd4a5f1eb9e070fe43c
russellpipal <russell.pipal@gmail.com>
2016-08-25 17:06:29 -0500

    Discovered ratios don't work correctly in proto-repl

--- src/learning_collections/core.clj
+++ src/learning_collections/core.clj
@@ -1,1 +1,15 @@
 (ns learning-collections.core)
+
+;; Comments come after two semicolons.
+
+(+ 1 1)
+;;returns 2
+
+(+ 1/2 1/3)
+;;returns 0.833333333333334
+;;Just discovered this is an error in proto-repl, the atom extension I'm using.
+;;Should return 1/6, which it does in the lein REPL.
+;;I submitted an issue to the proto-repl GitHib
+
+(str "Hello" " World!")
+;;returns "Hello World!"
